14831e898cf8a400c87aba4eb93615db86d98bb8
oliver-walker-hms <olwa@hms.se>
2023-08-18 14:14:56 -0400

    Add stub for payload manager

---
+++ src/main/java/com/hms_networks/sc/canary/data/CanaryDataPayloadManager.java
@@ -0,0 +1,20 @@
+package com.hms_networks.sc.canary.data;
+
+/**
+ * Class to manage generating payloads to send to the Canary API.
+ *
+ * @author HMS Networks, MU Americas Solution Center
+ * @since 1.0.0
+ */
+public class CanaryDataPayloadManager {
+
+  /**
+   * Gets the next available payload to send to Canary.
+   *
+   * @return A JSON payload for the tvqs field in the canary storeData request
+   * @since 1.0.0
+   */
+  public static String getNextPayload() {
+    return "";
+  }
+}
